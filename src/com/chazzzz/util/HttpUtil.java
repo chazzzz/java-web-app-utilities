4d4d8607d314394201a5a507d72f57d3ca86cf6f
chastineb1 <chastine@ideyatech.com>
2015-06-16 12:39:22 +0800

    implemented non-deprecated library of httpclient

---
+++ src/com/chazzzz/util/HttpUtil.java
@@ -0,0 +1,181 @@
+package com.chazzzz.util;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+/**
+ * Utility methods for sending requests to a url resource
+ * request methods such as post, get, delete and put returns HttpResponse
+ * Content can be easily resolved using to String HttpUtil.getContent
+ *
+ * @author chazz
+ *
+ */
+public class HttpUtil {
+
+	public static final String METHOD_GET = "GET";
+	public static final String METHOD_POST = "POST";
+	public static final String METHOD_DELETE = "DELETE";
+	public static final String METHOD_PUT = "PUT";
+
+	/**
+	 * Requests to a URL using the METHOD GET
+	 *
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public static HttpResponse get(String url, Map<String, String> params) throws IOException {
+		return HttpUtil.sendRequest(METHOD_GET, url, params, null);
+	}
+
+	/**
+	 * Requests to a URL using the METHOD GET
+	 *
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public static HttpResponse get(String url, Map<String, String> params, Map<String, String> header) throws IOException {
+		return HttpUtil.sendRequest(METHOD_GET, url, params, header);
+	}
+
+	/**
+	 * Requests to a URL using the METHOD POST
+	 *
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public static HttpResponse post (String url, Map<String, String> params) throws IOException {
+		return HttpUtil.sendRequest(METHOD_POST, url, params, null);
+	}
+
+	/**
+	 * Requests to a URL using the METHOD POST
+	 *
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public static HttpResponse post(String url, Map<String, String> headers, Map<String, String> params) throws ClientProtocolException, IOException {
+		return HttpUtil.sendRequest(METHOD_POST, url, params, headers);
+	}
+
+	/**
+	 * The core method of the utility function.
+	 *
+	 * @param method
+	 * @param requestUrl
+	 * @param params
+	 * @param headers
+	 * @return
+	 * @throws IOException
+	 */
+	private static HttpResponse sendRequest(String method, String requestUrl, Map<String, String> params, Map<String, String> headers) throws IOException {
+
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		HttpUriRequest request = null;
+
+		// if the method is GET, the supplied parameters will be constructed and appended to the URL
+		if (method.equals(METHOD_GET)) {
+			if (params != null && params.size() > 0) {
+				final StringBuilder rawUrl = new StringBuilder(requestUrl).append("?");
+				for (String paramKey : params.keySet()) {
+					rawUrl.append(paramKey)
+							.append("=")
+							.append(params.get(paramKey))
+							.append("&");
+				}
+				requestUrl = rawUrl.substring(0, rawUrl.length() - 1);
+			}
+			request = new HttpGet(requestUrl);
+
+		// if the method is POST, the parameters will be added using the NameValuePair of Apache
+		} else if (method.equals(METHOD_POST)) {
+			request = new HttpPost(requestUrl);
+			if (params != null && params.size() > 0) {
+				final List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+				for (String paramKey : params.keySet()) {
+					nameValuePair.add(new BasicNameValuePair(paramKey, params.get(paramKey)));
+				}
+
+				((HttpPost) request).setEntity(new UrlEncodedFormEntity(nameValuePair, "UTF-8"));
+			}
+
+		} else if (method.equals(METHOD_DELETE)) {
+			request = new HttpDelete(requestUrl);
+
+		} else if (method.equals(METHOD_PUT)) {
+			request = new HttpPut(requestUrl);
+			if (params != null && params.size() > 0) {
+				final ArrayList<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+				for (String paramKey : params.keySet()) {
+					nameValuePair.add(new BasicNameValuePair(paramKey, params.get(paramKey)));
+				}
+
+				((HttpPut) request).setEntity(new UrlEncodedFormEntity(nameValuePair, "UTF-8"));
+			}
+
+		} else {
+			throw new IllegalArgumentException("No request method is specified.");
+		}
+
+		// add the headers supplied
+		if(headers != null) {
+			for(String header: headers.keySet()) {
+				request.addHeader(header, headers.get(header));
+			}
+		}
+
+		return httpClient.execute(request);
+	}
+
+	public static HttpResponse postJson(String url, String jsonData, Map<String, String> headers) throws IOException {
+		HttpPost httpPost = new HttpPost(url);
+		StringEntity entity = new StringEntity(jsonData, "UTF-8");
+		entity.setContentType("application/json");
+
+		httpPost.setEntity(entity);
+
+		for(String key : headers.keySet()) {
+			httpPost.addHeader(new BasicHeader(key, headers.get(key)));
+		}
+
+		CloseableHttpClient client = HttpClients.createDefault();
+		return client.execute(httpPost);
+	}
+
+	public static HttpResponse delete(String url, Map<String, String> headers) throws IOException {
+		return sendRequest(METHOD_DELETE, url, null, headers);
+	}
+
+	public static String getContent(HttpResponse httpResponse) throws IOException {
+		return convertStreamToString(httpResponse.getEntity().getContent());
+	}
+
+	private static String convertStreamToString(InputStream inputStream) {
+		try {
+			return new Scanner(inputStream).useDelimiter("\\A").next();
+		} catch (NoSuchElementException e) {
+			return "";
+		}
+	}
+}
